6093f17508b3ab4eb9ca33d5073428ccf3d1deee
Natalie Teplicka <teplinat@fit.cvut.cz>
2025-11-16 15:53:38 +0100

    Finished Challenge 5

--- Prototype5/Assets/Challenge 5/Scripts/GameManagerX.cs
+++ Prototype5/Assets/Challenge 5/Scripts/GameManagerX.cs
@@ -9,6 +9,7 @@ public class GameManagerX : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI timeText;
     public GameObject titleScreen;
     public Button restartButton; 
 
@@ -17,20 +18,36 @@ public class GameManagerX : MonoBehaviour
     private int score;
     private float spawnRate = 1.5f;
     public bool isGameActive;
+    public int timeLimit = 60;
 
     private float spaceBetweenSquares = 2.5f; 
     private float minValueX = -3.75f; //  x value of the center of the left-most square
     private float minValueY = -3.75f; //  y value of the center of the bottom-most square
     
     // Start the game, remove title screen, reset score, and adjust spawnRate based on difficulty button clicked
-    public void StartGame()
+    public void StartGame(int difficulty)
     {
-        spawnRate /= 5;
+        spawnRate /= difficulty;
         isGameActive = true;
         StartCoroutine(SpawnTarget());
+        StartCoroutine(Countdown());
         score = 0;
         UpdateScore(0);
         titleScreen.SetActive(false);
+    }
+
+    IEnumerator Countdown()
+    {
+        int timeRemaining = timeLimit;
+
+        while (timeRemaining > 0 && isGameActive)
+        {
+            timeText.text = "Time: " + timeRemaining;
+            yield return new WaitForSeconds(1f);
+            timeRemaining--;
+        }
+
+        if (isGameActive) GameOver();
     }
 
     // While game is active spawn a random target
@@ -70,14 +87,14 @@ public class GameManagerX : MonoBehaviour
     public void UpdateScore(int scoreToAdd)
     {
         score += scoreToAdd;
-        scoreText.text = "score";
+        scoreText.text = "Score: " + score;
     }
 
     // Stop game, bring up game over text and restart button
     public void GameOver()
     {
         gameOverText.gameObject.SetActive(true);
-        restartButton.gameObject.SetActive(false);
+        restartButton.gameObject.SetActive(true);
         isGameActive = false;
     }
 
