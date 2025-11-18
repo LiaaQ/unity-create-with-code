5578f3b9a5233c7790fce94d1c79c99cb1b3f50f
Natalie Teplicka <teplinat@fit.cvut.cz>
2025-11-18 15:51:18 +0100

    Rewrote the script for better practices

---
+++ Prototype5/Debug Challenge/Assets/Scripts/DisplayText.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayText : MonoBehaviour
+{
+    public TextMesh text;
+    public ParticleSystem sparksParticles;
+
+    private List<string> textToDisplay = new List<string>();
+
+    [SerializeField] private float rotatingSpeed = 1.0f;
+
+    private int currentText = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textToDisplay.Add("Congratulations!");
+        textToDisplay.Add("All Errors Fixed");
+
+        text.text = textToDisplay[0];
+
+        sparksParticles.Play();
+        StartCoroutine(SwitchText());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, Time.deltaTime * rotatingSpeed);
+    }
+
+    IEnumerator SwitchText()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1.5f);
+            currentText++;
+            currentText %= 2;
+            text.text = textToDisplay[currentText];
+        }
+    }
+}
